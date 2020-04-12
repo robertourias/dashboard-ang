3d08fad62856d2f06fe34d6109f09545836e38b1
Roberto Nicoletti <roberto.urias@gmail.com>
2020-04-12 17:58:25 -0300

    inicio da implementação do componente de graficos

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DadosService } from './dados.service';
+
+declare var google: any;
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,9 +10,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  dados: any;
+
+  constructor(private dadosService: DadosService) { }
 
   ngOnInit(): void {
+    this.dadosService.obterDados().subscribe(
+      dados => {
+        this.dados = dados;
+        this.init();
+      }
+    );
+  }
+
+  init(): void {
+    if (typeof(google) !== 'undefined') {
+      google.charts.load('current', { package: ['corecharts'] });
+      setTimeout(() => {
+        google.charts.setOnLoadCallback(this.exibirGraficos());
+      }, 1000);
+    }
+  }
+
+  /**
+   * Método chamado assim que a API do google é inicializada
+   * Responsável por chamar os métodos geradores do graficos
+   * @return void
+   */
+  exibirGraficos(): void {
+    this.exibirPieChart();
+    // this.exibir3dPieChart();
+    // this.exibirBarChart();
+    // this.exibirLineChart();
+    // this.exibirColumnChart();
+    // this.exibirDonutChart();
+  }
+
+  /**
+   * Exibe o gráfico PieChart
+   * @return void
+   */
+  exibirPieChart(): void {
+    const el = document.getElementById('pir_chart');
+    // const chart = new google.visualization.PieChart(el);
+
+    // chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
+
+  /**
+   * Exibe o gráfico 3DPieChart
+   * @return void
+   */
+  exibir3DPieChart(): void {
+
+  }
+
+  /**
+   * Exibe o gráfico BarChart
+   * @return void
+   */
+  exibirBarChart(): void {
+
+  }
+
+  /**
+   * Exibe o gráfico LineChart
+   * @return void
+   */
+  exibirLineChart(): void {
+
+  }
+
+  /**
+   * Exibe o gráfico ColumnChart
+   * @return void
+   */
+  exibirColumnChart(): void {
+
+  }
+
+  /**
+   * Exibe o gráfico DonutChart
+   * @return void
+   */
+  exibirDonutChart(): void {
+
   }
 
 }
